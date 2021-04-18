076ca86e0898ae929e8e6824c61ef6eff9991b6c
CherifYasmine <yasmine.cherif9@gmail.com>
2021-04-18 02:22:30 +0100

    spaces crud

--- models/Space.js
+++ models/Space.js
@@ -1,15 +1,22 @@
 var mongoose = require("mongoose");
 
 const spaceSchema = mongoose.Schema({
-    name: String,
-    location: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
     description: String,
-    picture: String,
+    hourOpen: { type: Date },
+    hourClose: Date,
+    pictures: [String],
     joined: {
         type: Date,
-        default: new Date(),
+        default: Date.now,
     },
 });
 
-const space = mongoose.model("space", spaceSchema);
-export default space;
+module.exports = mongoose.model("Space", spaceSchema);
--- routes/spaces.js
+++ routes/spaces.js
@@ -1,7 +1,57 @@
 var express = require("express");
 var router = express.Router();
+const Space = require("../models/Space");
+router.get("/", async(req, res) => {
+    try {
+        const spaces = await Space.find();
+        res.status(200).json(spaces);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
+});
 
-router.get("/", (req, res) => {
-    res.send("We are on spaces");
+router.get("/:spaceId", async(req, res) => {
+    try {
+        const space = await Space.findById(req.params.spaceId);
+        res.status(200).json(space);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
+});
+
+router.post("/", async(req, res) => {
+    const space = new Space({
+        name: req.body.name,
+        location: req.body.location,
+        description: req.body.description,
+        pictures: req.body.pictures,
+    });
+    try {
+        const savedSpace = await space.save();
+        res.status(200).json(savedSpace);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
+});
+
+router.put("/", async(req, res) => {
+    try {
+        const updatedSpace = await Space.findByIdAndUpdate({ _id: req.body.spaceId },
+
+            req.body, { new: true }
+        );
+        res.status(200).json(updatedSpace);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
+});
+
+router.delete("/:spaceId", async(req, res) => {
+    try {
+        const removedSpace = await Space.remove({ _id: req.params.spaceId });
+        res.status(200).json(removedSpace);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
 });
 module.exports = router;
